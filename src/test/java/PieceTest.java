43e06b135380f8160cac1b4fa2d5104ae5f15630
Ji Hwan Yoon <yjh2865@naver.com>
2019-04-22 21:28:46 +0900

    add Piece Create Test

--- src/test/java/PieceTest.java
+++ src/test/java/PieceTest.java
@@ -8,5 +8,11 @@ public class PieceTest {
         Piece piece=new Piece(1,2,3);
         assertFalse(piece.x<0||piece.y<0);
     }
-
+    @Test
+    public void pieceCreateTest(){
+        Piece piece = new Piece(1,2,3);
+        assertThat(piece.y, is(1));
+        assertThat(piece.x, is(2));
+        assertThat(piece.count, is(3));
+    }
 }
