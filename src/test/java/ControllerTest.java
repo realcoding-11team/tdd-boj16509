5d7422046049d987b0fa86372d73530b8fd6cacf
potion <potionkr@gmail.com>
2019-04-22 20:48:53 +0900

    add checkNugNumber test

--- src/test/java/ControllerTest.java
+++ src/test/java/ControllerTest.java
@@ -30,4 +30,13 @@ public class ControllerTest {
         } catch(Exception e){}
 
     }
+
+    @Test(timeout = 1000)
+    public void main() {
+        Controller controller = new Controller();
+        Piece sang, king;
+        sang = new Piece(4, 2, 0);
+        king = new Piece(2, 5, 0);
+        System.out.print(controller.bfs(sang, king));
+    }
 }
