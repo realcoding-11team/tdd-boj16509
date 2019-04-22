7457bc163669d9078335ba0d4bacad77b07b8297
potion <potionkr@gmail.com>
2019-04-22 20:51:49 +0900

    add checkNegNumber test

--- src/test/java/PieceTest.java
+++ src/test/java/PieceTest.java
@@ -1,2 +1,12 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+
 public class PieceTest {
+    @Test
+    public void checkNegNumber(){
+        Piece piece=new Piece(1,2,3);
+        assertFalse(piece.x<0||piece.y<0);
+    }
+
 }
