b66f3f2bfff711fa52da0dceaba2e17778b47c20
willook@naver.com <willook@naver.com>
2019-04-22 21:10:38 +0900

    Remove unnecessary code in Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            BufferedWriter bw = new BufferedWriter(new FileWriter("./realtemp.txt"));
-        } catch(Exception e){}
 
         Scanner sc = new Scanner(System.in);
         Controller controller = new Controller();
