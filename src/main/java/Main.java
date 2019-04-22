e2b869e941bfcd904eb19f99a57d0060745fd6f6
willook@naver.com <willook@naver.com>
2019-04-22 20:23:58 +0900

    Upload data set & Add Test code for data set

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,14 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter("./realtemp.txt"));
+            bw.write("asdsads");
+        } catch(Exception e){}
+
         Scanner sc = new Scanner(System.in);
         Controller controller = new Controller();
         Piece sang, king;
