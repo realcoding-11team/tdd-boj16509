c77eb3a87bcde27cdda9e63830de7ad8fc11d4e6
gibeoom <deer9841@gmail.com>
2019-04-22 21:29:35 +0900

    add main test function

--- src/test/java/ControllerTest.java
+++ src/test/java/ControllerTest.java
@@ -61,4 +61,13 @@ public class ControllerTest {
         int ret = controller.bfs(sang,king);
         assertThat(ret, is(1));
     }
+    
+    @Test(timeout = 1000)
+    public void main() {
+        Controller controller = new Controller();
+        Piece sang, king;
+        sang = new Piece(4, 2, 0);
+        king = new Piece(2, 5, 0);
+        System.out.print(controller.bfs(sang, king));
+    }
 }
